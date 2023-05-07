f842fb089ce039382e436e56ebc2338f87529858
lingkerio <lingkerio@qq.com>
2023-05-07 16:31:04 +0800

    delete build folder

--- build/Debug/CMakeFiles/toy.dir/Download.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/toy.dir/Download.cpp.obj: D:\vscode\C++MinGW\src\Download.cpp \
- D:/vscode/C++MinGW/include/Download.hpp
--- build/Debug/CMakeFiles/toy.dir/Server.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/toy.dir/Server.cpp.obj: D:\vscode\C++MinGW\src\Server.cpp \
- D:/vscode/C++MinGW/include/server.hpp
